7e2e859a19c0c4fac3a4569b2e8e475657e8d509
choiwoochang <clllickme@naver.com>
2021-11-09 16:12:45 +0900

    WebView와 Web 사이의 상호작용

--- WebView/src/HomeScreen.js
+++ WebView/src/HomeScreen.js
@@ -1,11 +1,13 @@
-import React, {useState} from 'react';
-import { View, Text, SafeAreaView, StyleSheet, Alert } from 'react-native';
+import React, {useState, useRef} from 'react';
+import { View, Text, SafeAreaView, StyleSheet, Alert, Pressable } from 'react-native';
 import { WebView } from 'react-native-webview';
 import { Ionicons } from '@expo/vector-icons';
 
 const HomeScreen = () => {
 
-    const jsCode = `document.querySelector('.b').style.color='purple';`;
+    const [webRef, setWebRef] = useState(null);
+
+    const jsCode = `document.querySelector('.b').click();`;
     // 클래스가 b인 태그 찾아서 색상을 보라색으로 변경하는 코드
 
     const html = `
@@ -14,21 +16,33 @@ const HomeScreen = () => {
                 ReactNativeWebView.postMessage('hello react-native!!');
                 // 리액트 네이티브 웹 뷰로 데이터를 보내는 코드
             }
+
+
+            window.addEventListener('message', function(event) {
+                send();
+            });
         </script>
         <button class='b' onClick='send()'>Send</button>
     `;
 
+    const pressButton = () => {
+        webRef.postMessage("h");
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <WebView 
+                ref={(r) => setWebRef(r)}
                 style={styles.container}
                 source={{html}}
                 onMessage={(event) => Alert.alert(event.nativeEvent.data)}
                 javaScriptEnabled={true} // 자바스크립트를 사용할 수 있도록 ?
-                injectedJavaScript={jsCode} // 사용하고자 하는 자바스크립트 코드 ?
+                injectedJavaScript={jsCode} // 시작할 때 
             />
             <View style={styles.bottom}>
-                <Ionicons name="arrow-back" size={24} color='white'/>
+                <Pressable style={{backgroundColor: 'red'}} onPress={pressButton}>
+                    <Ionicons name="arrow-back" size={24} color='white'/>
+                </Pressable>
                 <Ionicons name="arrow-down" size={24} color='white'/>
                 <Ionicons name="arrow-forward" size={24} color='white'/>
             </View>
